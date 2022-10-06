c1411e1d34c68f9fa927bc990652ff5bee73b722
arya <sid19arya@gmail.com>
2022-10-06 01:01:24 -0400

    All tasks completed

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -39,7 +39,7 @@ public class Basics {
          *    (Relevant reading: 1.1.3. Printing things)
          */
         System.out.println(7 + 5);
-
+        System.out.println("Hello World!");
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
@@ -57,7 +57,7 @@ public class Basics {
          *
          *    (Relevant reading: 1.2. Variables and Types)
          */
-
+        int my_variable = 100;
         /*
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
@@ -86,7 +86,9 @@ public class Basics {
          *
          *    (Relevant reading: 1.8.2. for Loops)
          */
-
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
         /*
          * TODO (Task 3): Create a for-loop that loops from 10 to 0, printing
          *                the following at each iteration:
@@ -141,6 +143,11 @@ public class Basics {
          *                you may use them if you wish.
          */
         StringBuilder ret = new StringBuilder();
+        String[] split_array = to_split.split(" ");
+
+        for (String s : split_array) {
+            ret.append(s.charAt(0));
+        }
 
         // Fill in the rest of the body here
 
@@ -163,14 +170,20 @@ public class Basics {
      */
     public static int oddSum(int[] arr) {
         int current_sum = 0;
+        if (arr.length <= 1) {
+            return current_sum;
+        } else {
+            for (int i = 1; i < arr.length; i = i + 2) {
+                current_sum += arr[i];
+            }
 
-        /* TODO (Task 5): Complete this method body using a for-loop.
-         *                You can find the length of an array by using the
-         *                .length attribute (e.g. arr.length)
-         *                You can index into arrays as we do in Python
-         *                (e.g. arr[i] gives you the item at index i).
-         */
-
+            /* TODO (Task 5): Complete this method body using a for-loop.
+             *                You can find the length of an array by using the
+             *                .length attribute (e.g. arr.length)
+             *                You can index into arrays as we do in Python
+             *                (e.g. arr[i] gives you the item at index i).
+             */
+        }
         return current_sum;
     }
 
